318a00fa7b30ff933104d3ac8f085b9d8cb0df21
tiere <tommi@devlab.fi>
2015-03-18 11:14:00 +0200

    Lisätään scripti, joka lukee finvoiceja editilauksiksi

---
+++ finvoice_editilaukseksi.php
@@ -0,0 +1,75 @@
+<?php
+
+// Kutsutaanko CLI:stä
+if (php_sapi_name() != 'cli') {
+  die ("Tätä scriptiä voi ajaa vain komentoriviltä!\n");
+}
+
+date_default_timezone_set('Europe/Helsinki');
+
+if (trim($argv[1]) == '') {
+  die ("Et antanut yhtiötä!\n");
+}
+
+if (trim($argv[2]) == '') {
+  die ("Et antanut luettavien tiedostojen polkua!\n");
+}
+
+if (trim($argv[3]) == '') {
+  die ("Et antanut sähköpostiosoitetta!\n");
+}
+
+// lisätään includepathiin pupe-root
+ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . dirname(__FILE__));
+
+// otetaan tietokanta connect ja funktiot
+require "inc/connect.inc";
+require "inc/functions.inc";
+require "rajapinnat/edi.php";
+
+// Logitetaan ajo
+cron_log();
+
+// Sallitaan vain yksi instanssi tästä skriptistä kerrallaan
+pupesoft_flock();
+
+$yhtio = mysql_real_escape_string(trim($argv[1]));
+$yhtiorow = hae_yhtion_parametrit($yhtio);
+
+// Haetaan kukarow
+$query = "SELECT *
+          FROM kuka
+          WHERE yhtio = '{$yhtio}'
+          AND kuka = 'admin'";
+$kukares = pupe_query($query);
+
+if (mysql_num_rows($kukares) != 1) {
+  exit("VIRHE: Admin käyttäjä ei löydy!\n");
+}
+
+$kukarow = mysql_fetch_assoc($kukares);
+
+$path = trim($argv[2]);
+$path = substr($path, -1) != '/' ? $path . '/' : $path;
+
+$error_email = trim($argv[3]);
+
+$files = glob("{$path}/*.xml");
+
+foreach ($files as $file) {
+  $file = file_get_contents($file);
+  $xml = simplexml_load_string($file);
+
+  require "inc/verkkolasku-in-finvoice.inc";
+
+  $ovt_tunnus = $laskuttajan_ovt;
+  $verkkokauppa_asiakasnro = $yhtio;
+  $pupesoft_tilaustyyppi = "E";
+
+  $order = array(
+    "grand_total"  => $laskun_summa_eur,
+    "increment_id" => $laskun_numero
+  );
+
+  Edi::create($order);
+}
